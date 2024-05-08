75281f41a0eb56d2a07dd0dab197c15cacb8b872
oliver1 <oliver1@MacBook-Pro-de-Mac-2.local>
2024-05-08 09:16:53 -0500

    first commit

---
+++ lab8TareaCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lab8TareaCoreData
+//
+//  Created by oliver1 on 8/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
